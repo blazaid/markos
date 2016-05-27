0c3b3416c323cf5c31aa86292e2fd2cc8808364b
blazaid <alberto.da@gmail.com>
2016-05-27 20:09:32 +0200

    Bug fixed Example added

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO
+
+Well, yep, things to do
+
+* In grammars, terms like ZeroOrMore, OneToN and so, may be more useful if the
+number of returned elements depends on a random distribution specified at
+initialization time.
---
+++ examples/sentences.py
@@ -0,0 +1,18 @@
+import random
+
+from markos import MarkovChain
+
+if __name__ == '__main__':
+    mc = MarkovChain()
+    with open('frases.txt', 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        random.shuffle(lines)
+        for line in lines:
+            marks = '.', ',', ':', ';', '¡', '!', '?', '¿', '.', '.', '.',
+            for mark in marks:
+                line = line.replace(mark, ' {} '.format(mark))
+            result = line.lower().strip().split()
+            mc.train(result)
+    for e in mc.generate():
+        print(e, ' ', end='')
+    print('')
